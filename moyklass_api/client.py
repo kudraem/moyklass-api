90965f03fb93e71051ffa514dc509e9dafaa1768
Ivan Khimich <himivan@gmail.com>
2024-01-24 23:53:15 +0100

    add type annotation

--- moyklass_api/client.py
+++ moyklass_api/client.py
@@ -1,11 +1,12 @@
 import logging
 from enum import Enum
+from typing import Any, Dict, List
 
 import requests
 
 
 class MoyklassApiException(Exception):
-    def __init__(self, message=None):
+    def __init__(self, message: str = None) -> None:
         self.message = message
         super().__init__(message)
 
@@ -17,27 +18,35 @@ class PaymentOptype(Enum):
 
 
 class MoyklassApi:
-    def __init__(self, api_key, base_url="https://api.moyklass.com"):
+    def __init__(
+        self, api_key: str, base_url: str = "https://api.moyklass.com"
+    ) -> None:
         self.base_url = base_url
         self.api_key = api_key
         self.token = None
 
-    def set_token(self):
+    def set_token(self) -> None:
         data = {"apiKey": self.api_key}
         r = self._make_request("POST", "v1/company/auth/getToken", data=data)
         self.token = r["accessToken"]
 
-    def revoke_token(self):
+    def revoke_token(self) -> None:
         self._make_request("POST", "v1/company/auth/revokeToken")
         self.token = None
 
-    def _make_request(self, method, path, data=None, params=None):
+    def _make_request(
+        self,
+        method: str,
+        path: str,
+        data: Dict[str, Any] | None = None,
+        params: Dict[str, Any] | None = None,
+    ) -> Dict[str, Any]:
         url = f"{self.base_url}/{path}"
 
         headers = None
         if self.token is not None:
             headers = dict()
-            headers["x-access-token"] = self.token    
+            headers["x-access-token"] = self.token
 
         logging.debug(
             f"Sending {method} request to {url} with headers: {headers}; query params: {params}; data: {data}"
@@ -63,28 +72,28 @@ class MoyklassApi:
 
         return response_data
 
-    def __enter__(self):
+    def __enter__(self) -> "MoyklassApi":
         self.set_token()
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         self.revoke_token()
 
     def get_payments(
         self,
-        created_at=None,
-        date=None,
-        summa=None,
-        invoice_id=None,
-        optype=None,
-        payment_type_id=None,
-        include_user_subscriptions=False,
-        user_id=None,
-        filial_id=None,
-        append_invoices=False,
-        offset=0,
-        limit=100,
-    ):
+        created_at: List[str] | None = None,
+        date: List[str] | None = None,
+        summa: List[int] | None = None,
+        invoice_id: int | None = None,
+        optype: List[PaymentOptype] | None = None,
+        payment_type_id: int | None = None,
+        include_user_subscriptions: bool | None = False,
+        user_id: int | None = None,
+        filial_id: List[int] | None = None,
+        append_invoices: bool | None = False,
+        offset: int = 0,
+        limit: int = 100,
+    ) -> Dict[str, Any]:
         params = {}
         if created_at is not None:
             params["createdAt"] = created_at
@@ -131,6 +140,6 @@ class MoyklassApi:
 
         return self._make_request("GET", "v1/company/payments", params=params)
 
-    def get_user(self, user_id):
+    def get_user(self, user_id: int) -> Dict[str, Any]:
         path = f"v1/company/users/{user_id}"
         return self._make_request("GET", path)
