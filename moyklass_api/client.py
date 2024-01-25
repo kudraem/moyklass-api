01b3b204e168f830d15ebf0efa244506f1449b25
Ivan Khimich <himivan@gmail.com>
2024-01-25 01:03:17 +0100

    refactor and separate payments logic to own class

--- moyklass_api/client.py
+++ moyklass_api/client.py
@@ -1,6 +1,5 @@
 import logging
-from enum import Enum
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 import requests
 
@@ -15,12 +14,6 @@ class MoyklassApiException(Exception):
         """
         self.message = message
         super().__init__(message)
-
-
-class PaymentOptype(Enum):
-    INCOME = "income"
-    DEBIT = "debit"
-    REFUND = "refund"
 
 
 class MoyklassApi:
@@ -120,83 +113,6 @@ class MoyklassApi:
         Revokes the authentication token when exiting a context manager block.
         """
         self.revoke_token()
-
-    def get_payments(
-        self,
-        created_at: List[str] | None = None,
-        date: List[str] | None = None,
-        summa: List[int] | None = None,
-        invoice_id: int | None = None,
-        optype: List[PaymentOptype] | None = None,
-        payment_type_id: int | None = None,
-        include_user_subscriptions: bool | None = False,
-        user_id: int | None = None,
-        filial_id: List[int] | None = None,
-        append_invoices: bool | None = False,
-        offset: int = 0,
-        limit: int = 100,
-    ) -> Dict[str, Any]:
-        """
-        Retrieves payment information from the Moyklass API.
-
-        Args:
-            created_at (List[str], optional): List of creation dates. Defaults to None.
-            date (List[str], optional): List of payment dates. Defaults to None.
-            summa (List[int], optional): List of payment amounts. Defaults to None.
-            invoice_id (int, optional): Invoice ID. Defaults to None.
-            optype (List[PaymentOptype], optional): List of payment operation types. Defaults to None.
-            payment_type_id (int, optional): Payment type ID. Defaults to None.
-            include_user_subscriptions (bool, optional): Include user subscriptions in the response. Defaults to False.
-            user_id (int, optional): User ID. Defaults to None.
-            filial_id (List[int], optional): List of filial IDs. Defaults to None.
-            append_invoices (bool, optional): Append invoices to the response. Defaults to False.
-            offset (int, optional): Offset for pagination. Defaults to 0.
-            limit (int, optional): Limit for pagination. Defaults to 100.
-
-        Returns:
-            Dict[str, Any]: Response data from the Moyklass API.
-
-        Note:
-            https://api.moyklass.com/#tag/payments/paths/~1v1~1company~1payments/get
-        """
-        params = {}
-        if created_at is not None:
-            params["createdAt"] = created_at
-
-        if date is not None:
-            params["date"] = date
-
-        if summa is not None:
-            params["summa"] = summa
-
-        if invoice_id is not None:
-            params["invoiceId"] = invoice_id
-
-        if optype is not None:
-            decoded_optype = [
-                el.value for el in optype if isinstance(el, PaymentOptype)
-            ]
-            if decoded_optype:
-                params["optype"] = decoded_optype
-
-        if payment_type_id is not None:
-            params["paymentTypeId"] = payment_type_id
-
-        params["includeUserSubscriptions"] = str(include_user_subscriptions).lower()
-
-        if user_id is not None:
-            params["userId"] = user_id
-
-        if filial_id is not None:
-            params["filialId"] = filial_id
-
-        params["appendInvoices"] = str(append_invoices).lower()
-
-        params["offset"] = offset
-        params["limit"] = limit
-
-        return self._make_request("GET", "v1/company/payments", params=params)
-
     def get_user(self, user_id: int) -> Dict[str, Any]:
         """
         Retrieves user information from the Moyklass API.
