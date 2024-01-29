d4e10f7ec7970102487b3bc2495814ccf9dd065f
Ivan Khimich <himivan@gmail.com>
2024-01-29 22:03:43 +0100

    add get_users (GET /v1/company/users)

--- moyklass_api/user.py
+++ moyklass_api/user.py
@@ -1,6 +1,19 @@
-from typing import Any, Dict
+from enum import Enum
+from typing import Any, Dict, List
 
 from moyklass_api.client import MoyklassApi
+
+
+class UserSort(Enum):
+    ID = "id"
+    NAME = "name"
+    CREATED_AT = "createdAt"
+    UPDATED_AT = "updatedAt"
+
+
+class UserSortDirection(Enum):
+    ASC = "asc"
+    DESC = "desc"
 
 
 class User:
@@ -22,3 +35,57 @@ class User:
         """
         path = f"v1/company/users/{user_id}"
         return self.client._make_request("GET", path)
+
+    def get_users(
+        self,
+        created_at: List[str] | None = None,
+        updated_at: List[str] | None = None,
+        state_change_at: List[str] | None = None,
+        phone: str | None = None,
+        email: str | None = None,
+        name: str | None = None,
+        offset: int = 0,
+        limit: int = 100,
+        sort: UserSort = UserSort.ID,
+        sort_direction: UserSortDirection = UserSortDirection.ASC,
+        amoCRM_contact_id: int | None = None,
+        bitrix24_contact_id: int | None = None,
+        include_pay_link: bool = False,
+    ):
+        params = {}
+        if created_at is not None:
+            params["createdAt"] = created_at
+
+        if updated_at is not None:
+            params["updatedAt"] = updated_at
+
+        if state_change_at is not None:
+            params["stateChangeAt"] = state_change_at
+
+        if phone is not None:
+            params["phone"] = phone
+
+        if email is not None:
+            params["email"] = email
+
+        if name is not None:
+            params["name"] = name
+
+        params["offset"] = offset
+        params["limit"] = limit
+
+        if isinstance(sort, UserSort):
+            params["sort"] = sort.value
+
+        if isinstance(sort_direction, UserSortDirection):
+            params["sortDirection"] = sort_direction.value
+
+        if amoCRM_contact_id is not None:
+            params["amoCRMContactId"] = amoCRM_contact_id
+
+        if bitrix24_contact_id is not None:
+            params["bitrixContactId"] = bitrix24_contact_id
+
+        params["includePayLink"] = str(include_pay_link).lower()
+
+        return self.client._make_request("GET", "v1/company/users", params=params)
